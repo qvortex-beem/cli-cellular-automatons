fce52676658a66638de6fde7e06779300cc951d0
qvortex-beem <ghujfeuhh1@gmail.com>
2026-04-01 09:40:13 +0300

    first working prototype of the simulation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,142 @@
-mod control;
+mod world;
+use crate::world::World;
+
+use std::collections::HashMap;
+use std::io::{self, Stdout, Write};
+use std::time::Duration;
+
+use crossterm::event::{KeyCode, KeyEventKind, KeyModifiers};
+use crossterm::{
+    cursor::{self, MoveTo},
+    event::{
+        DisableBracketedPaste, DisableFocusChange, DisableMouseCapture, EnableBracketedPaste,
+        EnableFocusChange, EnableMouseCapture, Event, MouseButton, MouseEventKind, poll, read,
+    },
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
+
+pub fn control_simulation(world: &mut World) -> io::Result<()> {
+    let mut stdout = io::stdout();
+    enable_raw_mode()?;
+    execute!(
+        stdout,
+        EnableBracketedPaste,
+        EnableFocusChange,
+        EnableMouseCapture
+    )?;
+    loop {
+        if poll(Duration::from_millis(1000))? {
+            match read()? {
+                Event::Key(key) => {
+                    if key.kind == KeyEventKind::Press {
+                        match key.code {
+                            KeyCode::Char('q') | KeyCode::Char('Q') | KeyCode::Esc => break,
+                            KeyCode::Char('t') | KeyCode::Char('T') => {
+                                world.tick();
+                                world.draw(&mut stdout);
+                            }
+                            KeyCode::Char('d') => world.debug(),
+                            _ => (),
+                        }
+                    }
+                }
+                Event::Mouse(event) => {
+                    let x = event.column as i64;
+                    let y = event.row as i64;
+                    match event.kind {
+                        MouseEventKind::Down(button) => match button {
+                            MouseButton::Left => {
+                                world.animate_cell(x, y);
+                                world.draw(&mut stdout);
+                                // move_n_print(&mut stdout, event.column, event.row, '#');
+                            }
+                            MouseButton::Right => {
+                                world.kill_cell(x, y);
+                                world.draw(&mut stdout);
+                                // move_n_print(&mut stdout, event.column, event.row, ' ');
+                            }
+                            _ => (),
+                        },
+                        MouseEventKind::Drag(button) => match button {
+                            MouseButton::Left => match event.modifiers {
+                                KeyModifiers::CONTROL => {}
+                                _ => {
+                                    world.animate_cell(x, y);
+                                    world.draw(&mut stdout);
+                                }
+                            },
+                            MouseButton::Right => {
+                                world.kill_cell(x, y);
+                                world.draw(&mut stdout);
+                            }
+                            _ => (),
+                        },
+                        _ => (),
+                    }
+                }
+                _ => (),
+            }
+        }
+    }
+
+    execute!(
+        stdout,
+        DisableBracketedPaste,
+        DisableFocusChange,
+        DisableMouseCapture
+    )?;
+    disable_raw_mode()?;
+    Ok(())
+}
+
+pub fn move_n_print(out: &mut Stdout, x: u16, y: u16, char: char) {
+    execute!(out, MoveTo(x, y));
+    write!(out, "{}", char);
+    out.flush();
+}
+
+pub fn testing_read_event() -> io::Result<()> {
+    let mut stdout = io::stdout();
+    enable_raw_mode()?;
+    execute!(
+        stdout,
+        EnableBracketedPaste,
+        EnableFocusChange,
+        EnableMouseCapture
+    )?;
+
+    loop {
+        if poll(Duration::from_millis(2000))? {
+            match read()? {
+                Event::Key(key) if key.code == KeyCode::Esc => break,
+                Event::FocusGained => println!("FocusGained"),
+                Event::FocusLost => println!("FocusLost"),
+                Event::Key(event) => match event.kind {
+                    KeyEventKind::Press => println!("Key: {:?}", event),
+                    _ => (),
+                },
+                Event::Mouse(event) => println!("Mouse: {:?}", event),
+                #[cfg(feature = "bracketed-paste")]
+                Event::Paste(data) => println!("Pasted: {:?}", data),
+                Event::Resize(width, height) => println!("Size: {}x{}", width, height),
+                _ => (),
+            }
+        }
+    }
+
+    execute!(
+        stdout,
+        DisableBracketedPaste,
+        DisableFocusChange,
+        DisableMouseCapture
+    )?;
+    disable_raw_mode()?;
+    Ok(())
+}
 
 fn main() {
-    control::control_simulation();
-    //  second_file::testing_read_event();
+    let mut world = World::new();
+    control_simulation(&mut world);
+    // testing_read_event();
 }
---
+++ src/world.rs
@@ -0,0 +1,180 @@
+use crate::move_n_print;
+
+use crossterm::{
+    cursor::MoveTo,
+    execute,
+    terminal::{Clear, ClearType},
+};
+use std::{
+    collections::HashSet,
+    io::{self, Stdout, Write},
+};
+use std::{
+    collections::{HashMap, hash_map::Iter},
+    default,
+};
+
+const CHUNK_SIZE: i64 = 16;
+const SURVIVAL: usize = 2;
+const BIRTH: usize = 3;
+
+#[derive(Eq, Hash, PartialEq, Clone, Copy, Debug)]
+pub struct Cord {
+    pub x: i64,
+    pub y: i64,
+}
+
+#[derive(Eq, Hash, PartialEq, Debug)]
+struct ChunkCord {
+    chunk_x: i64,
+    chunk_y: i64,
+}
+#[derive(Debug, PartialEq)]
+enum Cell {
+    Alive,
+    Dead,
+}
+#[derive(Debug)]
+pub struct World {
+    chunks: HashMap<ChunkCord, HashMap<Cord, Cell>>,
+}
+impl World {
+    pub fn new() -> Self {
+        World {
+            chunks: HashMap::new(),
+        }
+    }
+
+    fn get_alive_neighbors_count(&self, cords: Cord) -> usize {
+        let mut count = 0;
+        for dx in -1..=1 {
+            for dy in -1..=1 {
+                if dx == 0 && dy == 0 {
+                    continue;
+                }
+                let nx = cords.x + dx;
+                let ny = cords.y + dy;
+                if self.is_cell_alive(nx, ny) {
+                    count += 1;
+                }
+            }
+        }
+        count
+    }
+
+    fn is_cell_alive(&self, x: i64, y: i64) -> bool {
+        let chunk_cord = self.get_chunk_cords(Cord { x, y });
+        let chunk = match self.chunks.get(&chunk_cord) {
+            Some(chunk) => chunk,
+            None => return false,
+        };
+
+        match chunk.get(&Cord { x, y }) {
+            Some(Cell::Alive) => true,
+            _ => false,
+        }
+    }
+
+    fn get_chunk_cords(&self, cords: Cord) -> ChunkCord {
+        ChunkCord {
+            chunk_x: cords.x / CHUNK_SIZE,
+            chunk_y: cords.y / CHUNK_SIZE,
+        }
+    }
+
+    fn should_be_alive(&self, x: i64, y: i64) -> bool {
+        let neighbors = self.get_alive_neighbors_count(Cord { x, y });
+        let is_alive = self.is_cell_alive(x, y);
+        (!is_alive && neighbors == BIRTH)
+            || (is_alive && (neighbors == SURVIVAL || neighbors == BIRTH))
+    }
+
+    pub fn animate_cell(&mut self, x: i64, y: i64) {
+        if x < 0 || y < 0 {
+            return;
+        }
+        let chunk_cord = self.get_chunk_cords(Cord { x, y });
+        let chunk = self.chunks.entry(chunk_cord).or_insert(HashMap::new());
+        chunk.insert(Cord { x, y }, Cell::Alive);
+    }
+
+    pub fn kill_cell(&mut self, x: i64, y: i64) {
+        if x < 0 || y < 0 {
+            return;
+        }
+        let chunk_cord = self.get_chunk_cords(Cord { x, y });
+        if let Some(chunk) = self.chunks.get_mut(&chunk_cord) {
+            chunk.remove(&Cord { x, y });
+        }
+    }
+
+    fn neighbors(&self, cords: Cord) -> Vec<Cord> {
+        let mut neighbors: Vec<Cord> = Vec::new();
+        for dx in -1..=1 {
+            for dy in -1..=1 {
+                if dx == 0 && dy == 0 {
+                    continue;
+                }
+                let nx = cords.x + dx;
+                let ny = cords.y + dy;
+                if nx < 0 || ny < 0 {
+                    continue;
+                }
+                neighbors.push(Cord { x: nx, y: ny });
+            }
+        }
+        return neighbors;
+    }
+
+    pub fn tick(&mut self) {
+        let mut to_check: HashSet<Cord> = HashSet::new();
+        for (chunk_cord, chunk) in self.chunks.iter() {
+            for cell_cord in chunk.keys() {
+                let neighbors = self.neighbors(cell_cord.clone());
+                for neighbor_cords in neighbors {
+                    to_check.insert(neighbor_cords);
+                }
+            }
+        }
+
+        let mut to_alive: HashMap<Cord, Cell> = HashMap::new();
+        for cord in to_check {
+            if self.should_be_alive(cord.x, cord.y) {
+                to_alive.insert(cord, Cell::Alive);
+            }
+        }
+
+        let mut new_chunks: HashMap<ChunkCord, HashMap<Cord, Cell>> = HashMap::new();
+        for cell_cord in to_alive.keys() {
+            let chunk_cord = self.get_chunk_cords(cell_cord.clone());
+            new_chunks
+                .entry(chunk_cord)
+                .or_insert(HashMap::new())
+                .insert(cell_cord.clone(), Cell::Alive);
+        }
+        self.chunks.clear();
+        self.chunks = new_chunks;
+    }
+
+    pub fn draw(&mut self, out: &mut Stdout) {
+        execute!(
+            out,
+            Clear(ClearType::Purge),
+            Clear(ClearType::All),
+            MoveTo(0, 0)
+        );
+        for chunk in self.chunks.values() {
+            for (cord, cell) in chunk {
+                // if *cell == Cell::Alive {
+                //     move_n_print(out, cord.x as u16, cord.y as u16, '#');
+                // }
+                move_n_print(out, cord.x as u16, cord.y as u16, '#');
+            }
+        }
+        out.flush();
+    }
+
+    pub fn debug(&mut self) {
+        print!("{:?}", self.chunks);
+    }
+}
