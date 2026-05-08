553232ed2f408ec3b07c6b95eadefa1ca781f627
qvortex-beem <ghujfeuhh1@gmail.com>
2026-05-08 15:58:45 +0300

    add focus-stack to ui

--- src/main.rs
+++ src/main.rs
@@ -18,123 +18,238 @@ use std::time::Duration;
 use std::{fs, vec};
 
 trait Component {
-    fn render(&self, frame: &mut Frame, area: Rect);
+    fn render(
+        &self,
+        frame: &mut Frame,
+        area: Rect,
+        focus_stack: Vec<usize>,
+        focus_index: usize,
+        has_focused_parent: bool,
+    );
     fn handle_event(&mut self, ev: &Event);
-}
-enum ScreenState {
-    Settings,
-    Simulation,
-}
-
-struct AppState {
-    screen: ScreenState,
-    cells_to_alive: usize,
-    cells_to_birth: usize,
-    simulation_speed: usize,
-}
-impl AppState {
-    fn new() -> Self {
-        Self {
-            screen: ScreenState::Settings,
-            cells_to_alive: 1,
-            cells_to_birth: 3,
-            simulation_speed: 1,
-        }
-    }
+    fn get_child(&mut self, index: usize) -> Option<&mut Box<dyn Component>>;
+    fn get_children_count(&mut self) -> usize;
+    fn get_component_type(&mut self) -> String;
 }
 
 struct App {
-    app_state: AppState,
-    event_bus: EventBus,
+    tip_text: String,
+    tip_focus_text: String,
+    focus_stack: Vec<usize>,
+    root: Box<dyn Component>,
 }
 impl App {
-    fn new() -> Self {
-        Self {
-            app_state: AppState::new(),
-            event_bus: EventBus {
-                queue: Vec::new(),
-                components: Vec::new(),
-            },
-        }
-    }
-}
-
-struct EventBus {
-    queue: Vec<Event>,
-    components: Vec<Box<dyn Component>>,
-}
-impl EventBus {
-    fn new(components: Vec<Box<dyn Component>>) -> Self {
-        Self {
-            queue: Vec::new(),
-            components: components,
-        }
-    }
-    fn dispatch(&mut self) {
-        for event in self.queue.drain(..) {
-            for component in self.components.iter_mut() {
-                component.handle_event(&event);
+    fn handle_event(&mut self, ev: &Event) {
+        if let Event::Key(key) = ev {
+            if key.kind == KeyEventKind::Press {
+                match key.code {
+                    KeyCode::Left | KeyCode::Up => {
+                        let siblings_len = self.get_focus_siblings();
+                        self.tip_text = siblings_len.to_string();
+                        self.move_focus(-1, siblings_len);
+                        self.tip_focus_text =
+                            self.focus_stack.iter().map(|int| int.to_string()).collect()
+                    }
+                    KeyCode::Down | KeyCode::Right => {
+                        let siblings_len = self.get_focus_siblings();
+                        self.tip_text = siblings_len.to_string();
+                        self.move_focus(1, siblings_len);
+                        self.tip_focus_text =
+                            self.focus_stack.iter().map(|int| int.to_string()).collect()
+                    }
+                    KeyCode::Enter => {
+                        let focus_stack = self.focus_stack.clone();
+                        let focus_component = self.get_focus_component(&focus_stack);
+                        if let Some(component) = focus_component {
+                            if component.get_children_count() > 0 {
+                                self.enter();
+                            }
+                        }
+                        self.tip_focus_text =
+                            self.focus_stack.iter().map(|int| int.to_string()).collect()
+                    }
+                    KeyCode::Esc => {
+                        if self.focus_stack.len() > 2 {
+                            self.esc();
+                            self.tip_focus_text =
+                                self.focus_stack.iter().map(|int| int.to_string()).collect()
+                        }
+                    }
+                    KeyCode::Char('c') => {
+                        let focus_stack = self.focus_stack.clone();
+                        let focus_component = self.get_focus_component(&focus_stack);
+                        if let Some(component) = focus_component {
+                            self.tip_text = component.get_children_count().to_string();
+                        }
+                    }
+                    KeyCode::Char('t') => {
+                        let focus_stack = self.focus_stack.clone();
+                        let focus_component = self.get_focus_component(&focus_stack);
+                        if let Some(component) = focus_component {
+                            self.tip_text = component.get_component_type();
+                        }
+                    }
+                    KeyCode::Char(ch) => {
+                        let focus_stack = self.focus_stack.clone();
+                        let focus_component = self.get_focus_component(&focus_stack);
+                        if let Some(component) = focus_component {
+                            component.handle_event(ev);
+                        }
+                    }
+                    _ => (),
+                }
             }
         }
     }
-    fn add_component(&mut self, component: Box<dyn Component>) {
-        self.components.push(component)
+    fn get_focus_component(&mut self, focus_stack: &[usize]) -> Option<&mut Box<dyn Component>> {
+        let mut current: &mut Box<dyn Component> = &mut self.root;
+        for i in &focus_stack.clone()[1..] {
+            current = current.get_child(*i)?;
+        }
+        Some(current)
+    }
+    fn current_index_mut(&mut self) -> Option<&mut usize> {
+        self.focus_stack.last_mut()
+    }
+    fn move_focus(&mut self, delta: isize, siblings_len: usize) {
+        if siblings_len == 0 {
+            return;
+        }
+        if let Some(i) = self.current_index_mut() {
+            let new_i = (*i as isize + delta).rem_euclid(siblings_len as isize) as usize;
+            *i = new_i;
+        }
+    }
+    fn enter(&mut self) {
+        self.focus_stack.push(0);
+    }
+    fn esc(&mut self) {
+        if self.focus_stack.len() > 1 {
+            self.focus_stack.pop();
+        }
+    }
+    fn get_focus_siblings(&mut self) -> usize {
+        let focus_stack = self.focus_stack.clone();
+        let parent_slice: &[usize] = &focus_stack[..focus_stack.len().saturating_sub(1)];
+        if let Some(parent) = self.get_focus_component(parent_slice) {
+            parent.get_children_count()
+        } else {
+            1
+        }
+    }
+    fn render(&mut self, frame: &mut Frame, area: Rect) {
+        let layout = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([Constraint::Fill(1), Constraint::Length(3)])
+            .split(area);
+        // "esc - назад |  ↑←→↓ - перемещение | enter - подтвердить"
+        let tip = Paragraph::new(format!(
+            "{} | {}",
+            self.tip_text.to_string(),
+            self.tip_focus_text.to_string()
+        ))
+        .alignment(Alignment::Left)
+        .wrap(Wrap { trim: true });
+        self.root
+            .render(frame, layout[0], self.focus_stack.clone(), 0, true);
+        frame.render_widget(tip, layout[1]);
     }
 }
 
 struct Container {
-    direction: Direction,
-    components: Vec<Box<dyn Component>>,
-}
-impl Container {
-    fn new(direction: Direction, components: Vec<Box<dyn Component>>) -> Self {
-        Self {
-            direction: direction,
-            components: components,
-        }
-    }
+    children: Vec<Box<dyn Component>>,
 }
 impl Component for Container {
     fn handle_event(&mut self, ev: &Event) {}
-    fn render(&self, frame: &mut Frame, area: Rect) {
-        let container = Block::default().borders(Borders::ALL);
+    fn render(
+        &self,
+        frame: &mut Frame,
+        area: Rect,
+        focus_stack: Vec<usize>,
+        focus_index: usize,
+        has_focused_parent: bool,
+    ) {
+        let is_focused =
+            !focus_stack.is_empty() && focus_stack[0] == focus_index && has_focused_parent;
+        let borders = if is_focused {
+            Borders::ALL
+        } else {
+            Borders::NONE
+        };
+        let focus_stack_to_children = {
+            if !focus_stack.is_empty() {
+                focus_stack.clone()[1..].to_vec()
+            } else {
+                Vec::new()
+            }
+        };
+        let container = Block::default().borders(borders);
         frame.render_widget(&container, area);
-        if self.components.is_empty() {
+        if self.children.is_empty() {
             return;
         }
         let inner_area = container.inner(area);
         let layout = Layout::default()
-            .direction(self.direction)
-            .constraints(std::iter::repeat(Constraint::Fill(1)).take(self.components.len()))
+            .direction(Direction::Horizontal)
+            .constraints(std::iter::repeat(Constraint::Fill(1)).take(self.children.len()))
             .split(inner_area);
-        for i in 0..self.components.len() {
-            self.components[i].render(frame, layout[i]);
+        for i in 0..self.children.len() {
+            self.children[i].render(
+                frame,
+                layout[i],
+                focus_stack_to_children.clone(),
+                i,
+                is_focused,
+            );
         }
+    }
+    fn get_child(&mut self, index: usize) -> Option<&mut Box<dyn Component>> {
+        self.children.get_mut(index)
+    }
+    fn get_children_count(&mut self) -> usize {
+        self.children.len()
+    }
+    fn get_component_type(&mut self) -> String {
+        "container".to_string()
     }
 }
 
 struct Label {
-    title: Option<String>,
     text: String,
-}
-impl Label {
-    fn new(title: Option<String>, text: String) -> Self {
-        Self { title, text }
-    }
 }
 impl Component for Label {
     fn handle_event(&mut self, ev: &Event) {}
-    fn render(&self, frame: &mut Frame, area: Rect) {
-        let text = self.text.clone();
-        let title = match self.title.clone() {
-            Some(title) => title,
-            _ => "".to_string(),
+    fn render(
+        &self,
+        frame: &mut Frame,
+        area: Rect,
+        focus_stack: Vec<usize>,
+        focus_index: usize,
+        has_focused_parent: bool,
+    ) {
+        let is_focused =
+            !focus_stack.is_empty() && focus_stack[0] == focus_index && has_focused_parent;
+        let borders = if is_focused {
+            Borders::ALL
+        } else {
+            Borders::NONE
         };
-        let label = Paragraph::new(text)
-            .block(Block::bordered().title(title))
+        let container = Block::default().borders(borders);
+        frame.render_widget(&container, area);
+        let inner_area = container.inner(area);
+        let label = Paragraph::new(self.text.clone())
             .alignment(Alignment::Left)
             .wrap(Wrap { trim: true });
-        frame.render_widget(label, area);
+        frame.render_widget(label, inner_area);
+    }
+    fn get_child(&mut self, index: usize) -> Option<&mut Box<dyn Component>> {
+        None
+    }
+    fn get_children_count(&mut self) -> usize {
+        1
+    }
+    fn get_component_type(&mut self) -> String {
+        "label".to_string()
     }
 }
 
@@ -146,41 +261,58 @@ fn main() -> color_eyre::Result<()> {
 
 fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
     let mut app = App {
-        app_state: AppState::new(),
-        event_bus: EventBus {
-            queue: Vec::new(),
-            components: vec![
-                Box::new(Container::new(
-                    Direction::Vertical,
-                    vec![
-                        Box::new(Label::new(None, String::from("text1"))),
-                        Box::new(Label::new(Some("title".to_string()), String::from("text2"))),
-                        Box::new(Label::new(None, String::from("text3"))),
-                        Box::new(Label::new(None, String::from("text4"))),
+        tip_text: "хз".to_string(),
+        tip_focus_text: "хз".to_string(),
+        focus_stack: vec![0, 0],
+        root: Box::new(Container {
+            children: vec![
+                Box::new(Container {
+                    children: vec![
+                        Box::new(Label {
+                            text: "лейбел1".to_string(),
+                        }),
+                        Box::new(Label {
+                            text: "лейбел".to_string(),
+                        }),
                     ],
-                )),
-                Box::new(Container::new(Direction::Vertical, vec![])),
-                Box::new(Container::new(
-                    Direction::Vertical,
-                    vec![
-                        Box::new(Container::new(
-                            Direction::Horizontal,
-                            vec![
-                                Box::new(Label::new(None, String::from("text1"))),
-                                Box::new(Label::new(None, String::from("text2"))),
+                }),
+                Box::new(Container {
+                    children: vec![
+                        Box::new(Container {
+                            children: vec![
+                                Box::new(Label {
+                                    text: "лейбел2".to_string(),
+                                }),
+                                Box::new(Label {
+                                    text: "лейбел3".to_string(),
+                                }),
                             ],
-                        )),
-                        Box::new(Container::new(Direction::Vertical, vec![])),
+                        }),
+                        Box::new(Container {
+                            children: vec![
+                                Box::new(Label {
+                                    text: "лейбел4".to_string(),
+                                }),
+                                Box::new(Label {
+                                    text: "лейбел5".to_string(),
+                                }),
+                            ],
+                        }),
                     ],
-                )),
+                }),
+                Box::new(Container {
+                    children: vec![Box::new(Label {
+                        text: "лейбел6".to_string(),
+                    })],
+                }),
             ],
-        },
+        }),
     };
 
     loop {
         terminal.draw(|frame| {
-            render(frame, &app);
-        });
+            render(frame, &mut app);
+        })?;
         let event = read()?;
         match event {
             Event::Key(key) => {
@@ -190,9 +322,8 @@ fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
                         | KeyCode::Char('Q')
                         | KeyCode::Char('й')
                         | KeyCode::Char('Й')
-                        | KeyCode::Esc
                         | KeyCode::Backspace => break,
-                        _ => (),
+                        _ => app.handle_event(&event),
                     }
                 }
             }
@@ -201,29 +332,8 @@ fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
     }
     Ok(())
 }
-fn render(frame: &mut Frame, app: &App) {
+
+fn render(frame: &mut Frame, app: &mut App) {
     let area = frame.area();
-    let layout = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints(vec![
-            Constraint::Fill(1),
-            Constraint::Fill(1),
-            Constraint::Fill(1),
-        ])
-        .split(area);
-    app.event_bus
-        .components
-        .get(0)
-        .unwrap()
-        .render(frame, layout[0]);
-    app.event_bus
-        .components
-        .get(1)
-        .unwrap()
-        .render(frame, layout[1]);
-    app.event_bus
-        .components
-        .get(2)
-        .unwrap()
-        .render(frame, layout[2]);
+    app.render(frame, area);
 }
